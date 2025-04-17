5009ed7e11f9773800df8d80447e23a52d0dca89
Edgars G <Edgars@gmail>
2025-04-17 12:43:08 +0300

    Atgriežam visus datus no posts tabulas

--- three.php
+++ three.php
@@ -1,11 +1,29 @@
 <?php
-$servername = "LocalHost";
-$username = "RalfsEgle";
-$password = "password";
-$dbname = "php27032025";
+$servername = "localhost";
+$username = "RalfsEgle"; // Jūsu datubāzes lietotājvārds
+$password = "password"; // Jūsu datubāzes parole
+$dbname = "php17042025"; // Jūsu datubāzes nosaukums
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
-    die("Savienojuma kļūda: " . $conn->connect_error);
+    die("Savienojums neizdevās: " . $conn->connect_error);
 }
+
+function getPosts($conn)
+{
+    $sql = "SELECT * FROM posts";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        $posts = [];
+        while ($row = $result->fetch_assoc()) {
+            $posts[] = $row;
+        }
+        return $posts;
+    } else {
+        return [];
+    }
+}
+
+$posts = getPosts($conn);
