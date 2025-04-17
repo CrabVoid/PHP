4659674896c69ef4b19030d11bfb72e8099fe5f1
Edgars G <Edgars@gmail>
2025-04-17 12:44:05 +0300

    Ar JOIN pievienojam komentārus

--- three.php
+++ three.php
@@ -27,3 +27,38 @@ function getPosts($conn)
 }
 
 $posts = getPosts($conn);
+
+function getPostsWithComments($conn)
+{
+    $sql = "
+        SELECT p.id as post_id, p.title, p.content as post_content, 
+               c.id as comment_id, c.content as comment_content, c.parent_comment_id
+        FROM posts p
+        LEFT JOIN comments c ON p.id = c.post_id
+        ORDER BY p.id, c.created_at";
+    $result = $conn->query($sql);
+
+    $data = [];
+    while ($row = $result->fetch_assoc()) {
+        $post_id = $row['post_id'];
+        if (!isset($data[$post_id])) {
+            $data[$post_id] = [
+                'title' => $row['title'],
+                'content' => $row['post_content'],
+                'comments' => []
+            ];
+        }
+
+        if ($row['comment_id']) {
+            $data[$post_id]['comments'][] = [
+                'id' => $row['comment_id'],
+                'content' => $row['comment_content'],
+                'parent_comment_id' => $row['parent_comment_id']
+            ];
+        }
+    }
+
+    return $data;
+}
+
+$posts_with_comments = getPostsWithComments($conn);
